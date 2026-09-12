d5dd76cbf48bcd49281657df570ace081adb5d29
Ro'zimurod Shoaxmatov <shoaxmatovrozimurod@gmail.com>
2026-09-12 16:47:39 +0500

    header

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const burgerBtn = document.getElementById('burgerBtn');
+const headerNav = document.querySelector('.header-nav');
+
+burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.toggle('active');
+    headerNav.classList.toggle('active');
+});
+
+document.querySelectorAll('.header-list-link').forEach(link => {
+    link.addEventListener('click', () => {
+        burgerBtn.classList.remove('active');
+        headerNav.classList.remove('active');
+    });
+});
